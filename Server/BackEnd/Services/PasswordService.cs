f3bed9c82a6c3455971f162aedba938d309f60b4
Ryan Keeley <ryankeeley2000@hotmail.co.uk>
2025-02-19 12:29:51 +0000

    added check againt common passwords

--- Server/BackEnd/Services/PasswordService.cs
+++ Server/BackEnd/Services/PasswordService.cs
@@ -2,13 +2,41 @@ namespace back_end.Services;
 
 public class PasswordService : IPasswordService
 {
+    private readonly HashSet<string> _commonPasswords;
+
+    public PasswordService()
+    {
+        _commonPasswords = LoadCommonPasswords();
+    }
+
     public bool IsPasswordInvalid(string password)
     {
-        return false;
+        return string.IsNullOrWhiteSpace(password) || password.Length < 8;
     }
-    
-    public bool IsPasswordCommon(string? password)
+
+    public bool IsPasswordCommon(string password)
     {
-        return false;
+        return _commonPasswords.Contains(password);
+    }
+
+    private HashSet<string> LoadCommonPasswords()
+    {
+        var hashSet = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data\\common-passwords.txt");
+
+        if (File.Exists(filePath))
+        {
+            foreach (var line in File.ReadLines(filePath))
+            {
+                hashSet.Add(line.Trim());
+            }
+        }
+
+        return hashSet;
     }
 }
+
+
+
+
+
